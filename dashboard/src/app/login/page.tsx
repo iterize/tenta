c6c77213858ec6a75f1ad1b21bae842513e16e5f
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-08-30 22:31:41 +0200

    Add artwork to login page

--- dashboard/src/app/login/page.tsx
+++ dashboard/src/app/login/page.tsx
@@ -6,6 +6,8 @@ import { Button } from "@/components/ui/button";
 import { useUser } from "@/requests/user";
 import { AuthLoadingScreen } from "@/components/custom/auth-loading-screen";
 import { redirect } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 
 // ------------
 
@@ -36,29 +38,54 @@ export default function Page() {
   }
 
   return (
-    <main className="flex flex-col items-center justify-center w-screen h-screen gap-y-6">
-      <div className="flex flex-col items-center w-full max-w-xs gap-y-2">
-        <h1 className="mb-1 text-2xl font-bold">Login</h1>
-        <Input
-          type="username"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <Input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button
-          className="mt-2"
-          onClick={isSubmitting ? () => {} : submit}
-          variant={isSubmitting ? "ghost" : "default"}
-        >
-          {isSubmitting ? "..." : "Login"}
-        </Button>
-      </div>
-    </main>
+    <>
+      <header className="absolute left-0 flex items-center justify-center w-screen px-6 h-14">
+        <h2 className="mb-1 text-xl font-bold">Tenta Dashboard</h2>
+        <div className="flex-grow" />
+        <p className="text-slate-800">
+          powered by{" "}
+          <Link
+            href="https://github.com/tum-esm/hermes"
+            target="_blank"
+            className="font-medium underline text-slate-950 hover:text-rose-600"
+          >
+            github.com/tum-esm/tenta
+          </Link>
+        </p>
+      </header>
+      <main className="grid w-screen h-screen grid-cols-2 grid-rows-1">
+        <div className="flex items-center justify-center">
+          <Image
+            src="/tenta-artwork-compressed.png"
+            alt="Tenta Artwork"
+            width="1716"
+            height="1716"
+            className="max-w-2xl"
+          />
+        </div>
+        <div className="flex flex-col items-center justify-center w-full max-w-xs mx-auto gap-y-2">
+          <h1 className="mb-1 text-2xl font-bold">Login</h1>
+          <Input
+            type="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            className="mt-2"
+            onClick={isSubmitting ? () => {} : submit}
+            variant={isSubmitting ? "ghost" : "default"}
+          >
+            {isSubmitting ? "..." : "Login"}
+          </Button>
+        </div>
+      </main>
+    </>
   );
 }
--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main className="flex h-screen w-screen items-center justify-center">
-      Homepage
-    </main>
-  );
+  redirect("/login");
 }
