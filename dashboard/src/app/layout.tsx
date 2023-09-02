c7567c13e73f28ddf5ccd161f83f650c4cc9c80f
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-09-02 11:07:11 +0200

    #9 Add message tray and add auth messages (1)
    Add message tray

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SWRProvider } from "@/app/swr-provider";
 import { NavigationBar } from "@/components/custom/navigation-bar";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
           <main className="flex flex-col w-screen min-h-screen">
             <NavigationBar />
             {children}
+            <Toaster position="bottom-right" reverseOrder={false} />
           </main>
         </SWRProvider>
       </body>
