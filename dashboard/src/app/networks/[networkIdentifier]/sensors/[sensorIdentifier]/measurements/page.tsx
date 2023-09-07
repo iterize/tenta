b7e26467cc7465baa4d4c3384e2198bf8780b43d
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-09-07 02:46:07 +0200

    #7 - Render individual sensor pages (17)
    Beautify measurements rendering

--- dashboard/src/app/networks/[networkIdentifier]/sensors/[sensorIdentifier]/measurements/page.tsx
+++ dashboard/src/app/networks/[networkIdentifier]/sensors/[sensorIdentifier]/measurements/page.tsx
@@ -9,6 +9,7 @@ import { Pagination } from "@/components/custom/pagination";
 import { Button } from "@/components/ui/button";
 import { useMeasurements } from "@/requests/measurements";
 import toast from "react-hot-toast";
+import { formatDistanceToNow } from "date-fns";
 
 export default function Page(props: {
   params: { networkIdentifier: string; sensorIdentifier: string };
@@ -77,28 +78,68 @@ export default function Page(props: {
       <div className="flex flex-row items-center justify-start w-full gap-x-6">
         <Pagination
           currentPageNumber={currentPageNumber}
-          numberOfPages={numberOfPages}
+          numberOfPages={
+            measurementsDataFetchingState === "fetching" ? 0 : numberOfPages
+          }
           setCurrentPageNumber={setCurrentPageNumber}
+          noDataPlaceholder={
+            measurementsDataFetchingState === "fetching" ? "..." : "no data"
+          }
         />
         <Button onClick={fetchMoreData}>load more data</Button>
       </div>
-      <div className="flex flex-col items-center justify-center w-full gap-y-6">
+      <div className="flex flex-col items-center justify-center w-full mt-2 gap-y-4">
         {measurementsData === undefined && "loading"}
-        {measurementsData !== undefined &&
-          measurementsData.map((measurement) => (
+        {measurementsData.map((measurement) => (
+          <div
+            key={JSON.stringify(measurement)}
+            className="flex flex-col justify-start w-full overflow-hidden text-sm bg-white border rounded-md shadow gap-x-6 border-slate-300"
+          >
             <div
               key={JSON.stringify(measurement)}
-              className="flex flex-row items-center justify-start w-full gap-x-6"
+              className="flex flex-row items-baseline justify-start w-full p-3 border-b border-slate-200"
             >
-              <div className="flex flex-row items-center justify-start w-full gap-x-6">
-                <div className="flex flex-col items-start justify-start w-full gap-y-0">
-                  <div>{measurement.creationTimestamp}</div>
-                  <div>{measurement.revision?.toString()}</div>
-                  <div>{JSON.stringify(measurement.value)}</div>
-                </div>
+              <div
+                className={
+                  "flex items-center flex-shrink-0 h-6 px-2 text-sm font-semibold text-blue-900 bg-blue-200 rounded mr-2 " +
+                  (measurement.revision === null
+                    ? "bg-slate-200 text-slate-700"
+                    : "bg-blue-200 text-blue-900")
+                }
+              >
+                {measurement.revision === null
+                  ? "No Config Revision"
+                  : `Config Revision ${measurement.revision.toString()}`}
+              </div>
+              <div className="font-medium">
+                Created{" "}
+                {formatDistanceToNow(
+                  new Date(measurement.creationTimestamp * 1000),
+                  {
+                    addSuffix: true,
+                  }
+                )}
+              </div>
+              <div className="flex-grow" />
+              <div>
+                {new Date(measurement.creationTimestamp * 1000).toISOString()}
               </div>
             </div>
-          ))}
+            <div className="w-full font-mono text-xs whitespace-pre bg-slate-100 text-slate-600">
+              {Object.entries(measurement.value).map(([key, value]) => (
+                <div
+                  key={key}
+                  className="flex flex-row items-center justify-start w-full px-3 py-1 border-b border-slate-200 gap-x-2"
+                >
+                  <div className="flex-grow-0 w-40 font-medium break-words whitespace-break-spaces">
+                    {key}:
+                  </div>
+                  <div>{JSON.stringify(value)}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
