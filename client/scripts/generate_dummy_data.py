dc895284be6ddf00756b9d41ff53d8958e987ad4
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-09-08 16:47:19 +0200

    #1 - Implement MQTT client library (2)
    Add warning logs to dummy data generation

--- client/scripts/generate_dummy_data.py
+++ client/scripts/generate_dummy_data.py
@@ -8,9 +8,10 @@ from wetterdienst.provider.dwd.observation import (
     DwdObservationParameter,
 )
 import json
+import polars as pl
 
 today = datetime.datetime.utcnow().date()
-settings = wetterdienst.Settings(ts_shape="long", ts_humanize=True, ts_si_units=True)
+settings = wetterdienst.Settings(ts_si_units=True)
 
 request = DwdObservationRequest(
     parameter=[
@@ -18,6 +19,7 @@ request = DwdObservationRequest(
         DwdObservationParameter.HOURLY.TEMPERATURE_AIR.TEMPERATURE_AIR_MEAN_200,
         DwdObservationParameter.HOURLY.WIND.WIND_DIRECTION,
         DwdObservationParameter.HOURLY.WIND.WIND_SPEED,
+        DwdObservationParameter.HOURLY.PRECIPITATION.PRECIPITATION_HEIGHT,
     ],
     resolution=DwdObservationResolution.HOURLY,
     start_date=(today - datetime.timedelta(days=28)).strftime("%Y-%m-%d"),
@@ -28,6 +30,14 @@ request = DwdObservationRequest(
 df = request.values.all().df
 df = df.drop("station_id", "dataset", "quality")
 
+wind_speed_warnings = df.filter(
+    (pl.col("parameter") == "wind_speed") & (pl.col("value") > 6)
+).drop("parameter", "value")
+rain_warnings = df.filter(
+    (pl.col("parameter") == "precipitation_height") & (pl.col("value") > 1)
+).drop("parameter", "value")
+
+dummy_measurement_records: list[dict] = []
 dummy_log_records: list[dict] = []
 
 for row in df.iter_rows():
@@ -38,21 +48,54 @@ for row in df.iter_rows():
     assert isinstance(time, datetime.datetime)
     assert isinstance(value, Optional[Union[int, float]]), type(value)
 
+    if value is not None:
+        dummy_measurement_records.append(
+            {
+                "sensor_identifier": "81bf7042-e20f-4a97-ac44-c15853e3618f",
+                "attribute": key,
+                "value": value,
+                "revision": None,
+                "creation_timestamp": time.timestamp(),
+                "receipt_timestamp": time.timestamp(),
+            }
+        )
+
+for row in wind_speed_warnings.iter_rows():
+    time = row[0]
+    assert isinstance(time, datetime.datetime)
+
     dummy_log_records.append(
         {
             "sensor_identifier": "81bf7042-e20f-4a97-ac44-c15853e3618f",
-            "attribute": key,
-            "value": value,
+            "severity": "warning",
+            "message": "High wind speed",
             "revision": None,
             "creation_timestamp": time.timestamp(),
             "receipt_timestamp": time.timestamp(),
-        }
+        },
+    )
+
+
+for row in rain_warnings.iter_rows():
+    time = row[0]
+    assert isinstance(time, datetime.datetime)
+
+    dummy_log_records.append(
+        {
+            "sensor_identifier": "81bf7042-e20f-4a97-ac44-c15853e3618f",
+            "severity": "warning",
+            "message": "Heavy rain",
+            "revision": None,
+            "creation_timestamp": time.timestamp(),
+            "receipt_timestamp": time.timestamp(),
+        },
     )
 
 with open("../server/tests/data.json", "r") as f:
     current_test_data = json.load(f)
 
-current_test_data["measurement"] += dummy_log_records
+current_test_data["measurement"] += dummy_measurement_records
+current_test_data["log"] += dummy_log_records
 
 with open("../server/tests/moritz_data.json", "w") as f:
     json.dump(current_test_data, f, indent=4)
