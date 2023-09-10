6d4970f8c6f679a02ffcac41e1c699e1647b4bc2
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-09-10 21:42:42 +0200

    #11 - Render measurements aggregation plots (2)
    Fetch measurement aggregations

--- dashboard/src/app/networks/[networkIdentifier]/sensors/[sensorIdentifier]/overview/page.tsx
+++ dashboard/src/app/networks/[networkIdentifier]/sensors/[sensorIdentifier]/overview/page.tsx
@@ -1,5 +1,28 @@
+"use client";
+
+import { AuthLoadingScreen } from "@/components/custom/auth-loading-screen";
+import { useMeasurementsAggregation } from "@/requests/measurements-aggregation";
+import { useUser } from "@/requests/user";
+import { redirect } from "next/navigation";
+
 export default function Page(props: {
   params: { networkIdentifier: string; sensorIdentifier: string };
 }) {
+  const { userData, userDataIsloading, logoutUser } = useUser();
+  const { measurementsAggregationData } = useMeasurementsAggregation(
+    userData?.accessToken,
+    logoutUser,
+    props.params.networkIdentifier,
+    props.params.sensorIdentifier
+  );
+
+  if (userDataIsloading || measurementsAggregationData === undefined) {
+    return <AuthLoadingScreen />;
+  } else if (userData === undefined) {
+    redirect("/login");
+  }
+
+  console.log(measurementsAggregationData);
+
   return "Hi";
 }
---
+++ dashboard/src/requests/measurements-aggregation.ts
@@ -0,0 +1,77 @@
+"use client";
+
+import useSWR from "swr";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { z } from "zod";
+import toast from "react-hot-toast";
+
+const schema = z.record(
+  z.string(),
+  z.array(
+    z
+      .object({
+        average: z.number(),
+        bucket_timestamp: z.string(),
+      })
+      .transform((obj) => ({
+        average: obj.average,
+        bucketTimestamp: new Date(obj.bucket_timestamp).getTime() / 1000,
+      }))
+  )
+);
+
+export type MeasurementsAggregationType = z.infer<typeof schema>;
+
+async function fetcher(
+  url: string,
+  accessToken: string | undefined,
+  logoutUser: () => void
+): Promise<MeasurementsAggregationType | undefined> {
+  if (!accessToken) {
+    return undefined;
+  }
+
+  return await axios
+    .get(`${process.env.NEXT_PUBLIC_SERVER_URL}${url}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+    .then((res: AxiosResponse) => schema.parse(res.data))
+    .catch((err: AxiosError) => {
+      console.error(`Error while fetching url ${url}: ${err}`);
+      if (err.response?.status === 401) {
+        toast("Session expired", { icon: "🔑" });
+        logoutUser();
+        window.location.reload();
+      } else if (err.response?.status.toString().startsWith("5")) {
+        toast("Server error", { icon: "🔥" });
+        logoutUser();
+        window.location.reload();
+      } else {
+        toast("Client error", { icon: "❓" });
+        logoutUser();
+        window.location.reload();
+      }
+      return undefined;
+    });
+}
+
+export function useMeasurementsAggregation(
+  accessToken: string | undefined,
+  logoutUser: () => void,
+  networkIdentifier: string,
+  sensorIdentifier: string
+) {
+  const { data } = useSWR(
+    [
+      `/networks/${networkIdentifier}/sensors/${sensorIdentifier}/measurements?aggregate=true`,
+      accessToken,
+    ],
+    ([url, accessToken]) => fetcher(url, accessToken, logoutUser)
+  );
+
+  return {
+    measurementsAggregationData: data,
+  };
+}
