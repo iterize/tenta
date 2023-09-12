8c34b4665e921c909afb55a21a2cab9fa488993a
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-09-13 00:37:11 +0200

    #1 - Implement MQTT client library (22)
    Extend example code with callbacks

--- client/example.py
+++ client/example.py
@@ -1,7 +1,12 @@
-import time
-import tenta
+# ## Basic Example
+#
+# **Create the client** and connect to the MQTT broker
 
-# Create the client instance and connect to the MQTT broker
+
+import time
+import typing
+import tenta
+import json
 
 tenta_client = tenta.TentaClient(
     mqtt_host="localhost",
@@ -12,8 +17,10 @@ tenta_client = tenta.TentaClient(
     revision=1,
 )
 
-# Publish logs asynchronously
-# -> your code continues to run while the message is being published
+
+# **Publish logs asynchronously**, i.e. your code continues to
+# run while the message is being published
+
 
 tenta_client.publish_log(
     severity="info",
@@ -28,8 +35,10 @@ tenta_client.publish_log(
 tenta_client.wait_for_publish()
 print("Logs 1-2 published!")
 
-# Publish logs synchronously
-# -> the code waits until the message has been published
+
+# **Publish logs synchronously**, i.e. ypur code waits until the
+# message has been published successfully
+
 
 tenta_client.publish_log(
     severity="warning",
@@ -39,7 +48,9 @@ tenta_client.publish_log(
 )
 print("Log 3 published!")
 
-# Publish measurements asynchronously
+
+# **Publish measurements asynchronously**
+
 
 tenta_client.publish_measurement(
     value={
@@ -47,12 +58,15 @@ tenta_client.publish_measurement(
         "humidity": 50.0,
         "pressure": 1013.25,
     },
+    # can be given a timestamp too
 )
 tenta_client.wait_for_publish()
 print("Measurements published!")
 
-# Publish acknowledgements asynchronously
-# -> "did the sensor successfully process a new revision?" -> true/false
+
+# **Publish acknowledgements asynchronously**, i.e. "did the sensor successfully
+# process a new config revision?" -> true/false
+
 
 tenta_client.publish_acknowledgement(
     success=False,
@@ -60,11 +74,51 @@ tenta_client.publish_acknowledgement(
 )
 tenta_client.publish_acknowledgement(
     success=True,
-    # uses the revision given to the client on initialization (line 11)
+    # uses the revision given to the client on initialization (above)
 )
 tenta_client.wait_for_publish()
 print("Acknowledgements published!")
 
-# tear down MQTT connection
+
+# Get the **latest received config** on demand
+
+
+config_message: typing.Optional[
+    tenta.ConfigMessageDict
+] = tenta_client.get_latest_received_config_message()
+
+
+# **Tear down** MQTT connection
+
+
+tenta_client.teardown()
+
+
+# ## Advanced Example Using Callbacks
+#
+# You can gice callbacks to the client to get notified when a message has
+# been published successfully or when the client receives a new config message.
+#
+# You can also check the current length of the clients message queue =
+# number of messages that have not been published yet. When offline, a
+# full queue might raise Exceptions.
+
+
+tenta_client = tenta.TentaClient(
+    mqtt_host="localhost",
+    mqtt_port=1883,
+    mqtt_identifier="server",
+    mqtt_password="password",
+    sensor_identifier="81bf7042-e20f-4a97-ac44-c15853e3618f",
+    revision=1,
+    on_config_message=lambda config_message: print(
+        f"New config message: {json.dumps(config_message)}"
+    ),
+    on_publish=lambda message_id: print(
+        f"Message with id {message_id} has been published!"
+    ),
+)
+
+current_queue_length = tenta_client.get_active_message_count()
 
 tenta_client.teardown()
