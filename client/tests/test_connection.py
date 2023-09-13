381656944d5e113a056bb2c6e9dbde3ae5cd0957
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-09-13 02:00:53 +0200

    #1 - Implement MQTT client library (27)
    Test TLS support in connection tests

--- client/tests/test_connection.py
+++ client/tests/test_connection.py
@@ -1,93 +1,90 @@
+import os
+import ssl
+import typing
 import tenta
 
-MQTT_HOST = "test.mosquitto.org"
-MQTT_PORT = 1884
-MQTT_IDENTIFIER = "rw"
-MQTT_PASSWORD = "readwrite"
-CONNECTION_TIMEOUT = 3
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MQTT_BROKERS: typing.List[typing.Dict[str, typing.Any]] = [
+    {
+        "mqtt_host": "test.mosquitto.org",
+        "mqtt_port": 8885,
+        "mqtt_identifier": "rw",
+        "mqtt_password": "readwrite",
+        "tls_parameters": tenta.TLSParameters(
+            ca_certs=os.path.join(PROJECT_DIR, "tests/mosquitto.org.crt"),
+            cert_reqs=ssl.CERT_REQUIRED,
+            tls_version=ssl.PROTOCOL_TLSv1_2,
+        ),
+    },
+    {
+        "mqtt_host": "test.mosquitto.org",
+        "mqtt_port": 1884,
+        "mqtt_identifier": "rw",
+        "mqtt_password": "readwrite",
+    },
+]
+DEFAULTS: typing.Dict[str, typing.Any] = {
+    "sensor_identifier": "some-sensor-id",
+    "revision": 17,
+    "connection_timeout": 1,
+}
 
 
 def test_successful_connection() -> None:
-    tenta_client = tenta.TentaClient(
-        mqtt_host=MQTT_HOST,
-        mqtt_port=MQTT_PORT,
-        mqtt_identifier=MQTT_IDENTIFIER,
-        mqtt_password=MQTT_PASSWORD,
-        sensor_identifier="some-sensor-id",
-        revision=1,
-        connection_timeout=CONNECTION_TIMEOUT,
-    )
-
-    tenta_client.teardown()
+    for broker in MQTT_BROKERS:
+        tenta.TentaClient(
+            **broker,
+            **DEFAULTS,
+        ).teardown()
 
 
 def test_bad_connection_with_wrong_host() -> None:
-    try:
-        tenta_client = tenta.TentaClient(
-            mqtt_host=f"notadomain.{MQTT_HOST}",
-            mqtt_port=MQTT_PORT,
-            mqtt_identifier=MQTT_IDENTIFIER,
-            mqtt_password=MQTT_PASSWORD,
-            sensor_identifier="some-sensor-id",
-            revision=1,
-            connection_timeout=CONNECTION_TIMEOUT,
-        )
-        tenta_client.teardown()
-    except Exception:
-        return
+    for broker in MQTT_BROKERS:
+        try:
+            tenta.TentaClient(
+                **{**broker, "mqtt_host": "notadomain"},
+                **DEFAULTS,
+            ).teardown()
+        except Exception:
+            return
 
-    raise Exception("Should have raised an exception")
+        raise Exception("Should have raised an exception")
 
 
 def test_bad_connection_with_wrong_port() -> None:
-    try:
-        tenta_client = tenta.TentaClient(
-            mqtt_host=MQTT_HOST,
-            mqtt_port=MQTT_PORT + 1,
-            mqtt_identifier=MQTT_IDENTIFIER,
-            mqtt_password=MQTT_PASSWORD,
-            sensor_identifier="some-sensor-id",
-            revision=1,
-            connection_timeout=CONNECTION_TIMEOUT,
-        )
-        tenta_client.teardown()
-    except ConnectionError:
-        return
+    for broker in MQTT_BROKERS:
+        try:
+            tenta.TentaClient(
+                **{**broker, "mqtt_port": 1234},
+                **DEFAULTS,
+            ).teardown()
+        except ConnectionError:
+            return
 
-    raise Exception("Should have raised a ConnectionError")
+        raise Exception("Should have raised a ConnectionError")
 
 
 def test_bad_connection_with_wrong_identifier() -> None:
-    try:
-        tenta_client = tenta.TentaClient(
-            mqtt_host=MQTT_HOST,
-            mqtt_port=MQTT_PORT,
-            mqtt_identifier=MQTT_IDENTIFIER + "...",
-            mqtt_password=MQTT_PASSWORD,
-            sensor_identifier="some-sensor-id",
-            revision=1,
-            connection_timeout=CONNECTION_TIMEOUT,
-        )
-        tenta_client.teardown()
-    except ConnectionError:
-        return
+    for broker in MQTT_BROKERS:
+        try:
+            tenta.TentaClient(
+                **{**broker, "mqtt_identifier": "..."},
+                **DEFAULTS,
+            ).teardown()
+        except ConnectionError:
+            return
 
-    raise Exception("Should have raised a ConnectionError")
+        raise Exception("Should have raised a ConnectionError")
 
 
 def test_bad_connection_with_wrong_password() -> None:
-    try:
-        tenta_client = tenta.TentaClient(
-            mqtt_host=MQTT_HOST,
-            mqtt_port=MQTT_PORT,
-            mqtt_identifier=MQTT_IDENTIFIER,
-            mqtt_password=MQTT_PASSWORD + "...",
-            sensor_identifier="some-sensor-id",
-            revision=1,
-            connection_timeout=CONNECTION_TIMEOUT,
-        )
-        tenta_client.teardown()
-    except ConnectionError:
-        return
+    for broker in MQTT_BROKERS:
+        try:
+            tenta.TentaClient(
+                **{**broker, "mqtt_password": "..."},
+                **DEFAULTS,
+            ).teardown()
+        except ConnectionError:
+            return
 
-    raise Exception("Should have raised a ConnectionError")
+        raise Exception("Should have raised a ConnectionError")
