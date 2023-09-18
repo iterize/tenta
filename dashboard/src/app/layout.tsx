743a4fbd7d1cbe06cb059653844d6bdb9bdb1e9a
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-09-19 01:30:08 +0200

    Finish dashboard
    add metadata, og images and favicon

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -8,8 +8,17 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://someridiculousdomaintogetridofthaterror.com"),
   title: "Tenta Dashboard",
   description: "Remote and real-time management of distributed sensor networks",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://github.com/iterize/tenta",
+    title: "Tenta Dashboard",
+    description:
+      "Remote and real-time management of distributed sensor networks",
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +28,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:image" content="/tenta-favicon-1024.png" />
+        <meta property="og:image:width" content="1024" />
+        <meta property="og:image:height" content="1024" />
+        <meta property="og:image:alt" content="Tenta Dashboard Favicon" />
+        <meta property="og:image" content="/tenta-dashboard-og-1024-512.png" />
+        <meta property="og:image:width" content="1024" />
+        <meta property="og:image:height" content="512" />
+        <meta property="og:image:alt" content="Tenta Dashboard Banner" />
+        <meta property="og:image" content="/tenta-dashboard-og-1200-630.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Tenta Dashboard Banner" />
+      </head>
       <body className={inter.className}>
         <SWRProvider>
           <main className="flex flex-col w-screen min-h-screen">
